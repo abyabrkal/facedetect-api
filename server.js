cdbb223584bef187df96be0e0072268618539aaa
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-04-11 14:51:41 +0400

    minor updates to code.

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/signin', (req, res) => {
 
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
-            res.json('success');
+            res.json(database.user[0]);
     } else {
         res.status(400).json('Error logging in');
     }
